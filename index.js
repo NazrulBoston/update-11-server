0263cf3208f619c2e45955f38fc5279466cc9a7a
Nazrul Islam <dobon.islam@icloud.com>
2024-12-22 21:39:09 -0500

    code added

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express()
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const email = require('surge/lib/middleware/email');
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
 const port = process.env.PORT || 3000
 
-
 //middleware
-
 const corOptions = {
     origin: [
         'http://localhost:5173',
@@ -19,11 +21,28 @@ const corOptions = {
     optionSuccessStatus: 200,
 }
 app.use(cors(corOptions))
-
 // app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
+ 
 
-
+// verify jwt middleware
+const verifyToken = (req, res, next ) => {
+    const token = req.cookies?.token
+    if(!token) return res.status(401).send({message:'unauthorized access'})
+        if(token){
+            jwt.verify(token, ACCESS_TOKEN_SECRET, (err, decoded)=> {
+                if(err){
+                    console.log(err)
+                    return res.status(401).send({message:'unauthorized access'})
+                }
+                console.log(decoded)
+                // akhnane req.user ar moddhe decoded token dile onno api ar moddhe theke user ar token aceess kara jabe
+                req.user = decoded
+                next();
+            })
+        }
+}
 
 
 
@@ -44,6 +63,35 @@ async function run() {
         const jobsCollection = client.db('soloSpehere').collection('jobs')
         const bidsCollection = client.db('soloSpehere').collection('bids')
 
+        // jwt generate 
+        app.post('/jwt', async (req, res) => {
+            const email = req.body
+            const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '365d'
+            })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : "strict",
+            })
+                .send({ success: true })
+
+        })
+ 
+
+        // clear token on logout
+        app.get('/logout', (req, res) => {
+            res.clearCookie('token', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                maxAge: 0
+
+            })
+                .send({ success: true })
+        })
+
+
         //get all the jobs data from db
         app.get("/jobs", async (req, res) => {
             const result = await jobsCollection.find().toArray();
@@ -61,12 +109,20 @@ async function run() {
         })
 
 
-
         //save a bid data in DB or post a bid data in DB
         app.post("/bid", async (req, res) => {
             const bidData = req.body;
-            // console.log(bidData)
-            // return
+            // check if it is a duplicate request
+            const query = {
+                email: bidData.email,
+                jobId : bidData.jobId
+            }
+            const alreadyApplied = await bidsCollection.findOne(query)
+            console.log(alreadyApplied)
+
+            if(alreadyApplied){
+                return res.status(400).send('You already applied!')
+            }
             const result = await bidsCollection.insertOne(bidData)
             res.send(result);
         })
@@ -81,9 +137,15 @@ async function run() {
             res.send(result);
         })
 
+
         //get all jobs posted by a specific user
-        app.get('/jobs/:email', async (req, res) => {
+        app.get('/jobs/:email', verifyToken, async (req, res) => {
+            const tokenEmail = req.user?.email
+            console.log(tokenEmail)
             const email = req.params.email
+            if(tokenEmail !== email ){
+                return res.status(403).send({message:'forbidden access'})
+            }
             const query = { 'buyer.email': email }
             const result = await jobsCollection.find(query).toArray()
             res.send(result)
@@ -117,17 +179,17 @@ async function run() {
 
 
         //gets all bids for a user by email from db
-        app.get('/my-bids/:email', async (req, res) => {
+        app.get('/my-bids/:email', verifyToken, async (req, res) => {
             const email = req.params.email
-            // const query = { email: email }
-            const query = { email }
+            const query = { email: email }
+            // const query = { email }
             const result = await bidsCollection.find(query).toArray()
             res.send(result)
         })
 
 
         //gets all bid requests from db for job owner
-        app.get('/bid-requests/:email', async (req, res) => {
+        app.get('/bid-requests/:email',verifyToken, async (req, res) => {
             const email = req.params.email
             // in db buyer is a object so objects value need to access with quotation 
             const query = { 'buyer.email': email }
